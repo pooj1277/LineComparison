297b63bca61ccf61b0647fee8641a99529f77b43
pooj1277 <pooja.sd3@gmail.com>
2021-09-16 10:02:08 -0700

    Welcome to Line Comparison Computation Program

---
+++ src/main/java/com/lineComparison/App.java
@@ -0,0 +1,12 @@
+/**
+ * Welcome to Line Comparison Computation Program
+ *
+ */
+package com.lineComparison;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparison Computation Program" );
+    }
+}
